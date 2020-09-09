4f2d2ce6daa1996ba81ebdd9f5d53f0013dd403d
unknown <miha.lebaric@gmail.com>
2020-09-09 23:49:54 +0200

    first commit

---
+++ PlatformExcercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlatformExcercise.MvcApplication" Language="C#" %>
---
+++ PlatformExcercise/Models/OpombaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlatformExcercise.Models
+{
+    public class OpombaModel
+    {
+        public int Id { get; set; }
+        public string Opomba { get; set; }
+        public DateTime DatumOpombe { get; set; }
+    }
+}
